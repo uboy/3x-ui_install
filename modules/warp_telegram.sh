36c7722cf00568b128934e920285cf14f56a5861
Denis Mazur <uboy@list.ru>
2026-09-11 15:59:26 +0300

    fix(warp): remove Google IPs from WARP routing to prevent YouTube throttling

--- modules/warp_telegram.sh
+++ modules/warp_telegram.sh
@@ -42,16 +42,15 @@ module_warp_telegram_install() {
 
   local TELEGRAM_IPS="91.105.192.0/23, 91.108.4.0/22, 91.108.8.0/22, 91.108.12.0/22, 91.108.16.0/22, 91.108.20.0/22, 91.108.56.0/22, 149.154.160.0/20, 185.76.151.0/24"
   
-  # Динамически получаем IP-адреса Google (включая YouTube)
-  log "Получение актуальных IP-адресов Google/YouTube..."
-  local GOOGLE_IPS
-  if GOOGLE_IPS=$(curl -sSLf https://www.gstatic.com/ipranges/goog.json | grep -o '"ipv4Prefix": "[^"]*"' | cut -d '"' -f 4 | paste -sd, -); then
-    if [[ -n "$GOOGLE_IPS" ]]; then
-      TELEGRAM_IPS="${TELEGRAM_IPS}, ${GOOGLE_IPS}"
-    fi
-  else
-    warn "Не удалось получить IP-адреса Google. Обход DPI для YouTube может работать не полностью."
-  fi
+  # Динамически получаем IP-адреса Google (Отключено: WARP сильно режет скорость YouTube)
+  # local GOOGLE_IPS
+  # if GOOGLE_IPS=$(curl -sSLf https://www.gstatic.com/ipranges/goog.json | grep -o '"ipv4Prefix": "[^"]*"' | cut -d '"' -f 4 | paste -sd, -); then
+  #   if [[ -n "$GOOGLE_IPS" ]]; then
+  #     TELEGRAM_IPS="${TELEGRAM_IPS}, ${GOOGLE_IPS}"
+  #   fi
+  # else
+  #   warn "Не удалось получить IP-адреса Google. Обход DPI для YouTube может работать не полностью."
+  # fi
   
   # Удаляем замену DNS
   sed -i 's/^DNS = /#DNS = /' wgcf-profile.conf
@@ -90,7 +89,8 @@ EOF
   cat << 'EOF' > "$CRON_SCRIPT"
 #!/usr/bin/env bash
 TELEGRAM_IPS="91.105.192.0/23, 91.108.4.0/22, 91.108.8.0/22, 91.108.12.0/22, 91.108.16.0/22, 91.108.20.0/22, 91.108.56.0/22, 149.154.160.0/20, 185.76.151.0/24"
-GOOGLE_IPS=$(curl -sSLf https://www.gstatic.com/ipranges/goog.json | grep -o '"ipv4Prefix": "[^"]*"' | cut -d '"' -f 4 | paste -sd, -)
+# Google IPs отключены: Cloudflare WARP сильно режет скорость видеотрафика YouTube
+GOOGLE_IPS=""
 if [[ -n "$GOOGLE_IPS" ]]; then
   ALL_IPS="${TELEGRAM_IPS}, ${GOOGLE_IPS}"
   sed -i "s|^AllowedIPs.*|AllowedIPs = $ALL_IPS|" /etc/wireguard/warp.conf
